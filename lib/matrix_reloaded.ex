918f8a97891b6a65d4a9435b89462904de456c2f
Ondrej Tucek <ondrej.tucek@gmail.com>
2019-01-07 21:24:56 +0100

    Refactored new function and fixed a few commentaries

--- lib/matrix_reloaded.ex
+++ lib/matrix_reloaded.ex
@@ -10,15 +10,20 @@ defmodule MatrixReloaded do
   Each matrix is represented as a "list of lists" and most functions return
   [Result](https://hexdocs.pm/result/api-reference.html). It means either tuple
   of `{:ok, element}` or `{:error, "msg"}` where `element` is type,
-  (see module [Matrix](https://hexdocs.pm/matrix_reloaded/MatrixReloaded.Matrix.html#t:element/0)).
+  (see module Matrix).
+
+  Numbering of row and column of matrix starts from `0` and goes to `m - 1`
+  and `n - 1` where `{m, n}` is dimension (size) of matrix. Similarly for
+  a row or column vector.
 
   ## Examples:
+      iex> alias MatrixReloaded.Matrix
 
-      iex> up = MatrixReloaded.Matrix.diag([2, 2, 2], 1)
-      iex> down = MatrixReloaded.Matrix.diag([2, 2, 2], -1)
-      iex> diag = MatrixReloaded.Matrix.diag([3, 3, 3, 3])
-      iex> band_mat = Result.and_then_x([up, down], &MatrixReloaded.Matrix.add(&1, &2))
-      iex> band_mat = Result.and_then_x([band_mat, diag], &MatrixReloaded.Matrix.add(&1, &2))
+      iex> up = Matrix.diag([2, 2, 2], 1)
+      iex> down = Matrix.diag([2, 2, 2], -1)
+      iex> diag = Matrix.diag([3, 3, 3, 3])
+      iex> band_mat = Result.and_then_x([up, down], &Matrix.add(&1, &2))
+      iex> band_mat = Result.and_then_x([band_mat, diag], &Matrix.add(&1, &2))
       {:ok,
         [
           [3, 2, 0, 0],
@@ -28,8 +33,8 @@ defmodule MatrixReloaded do
         ]
       }
 
-      iex> ones = MatrixReloaded.Matrix.new(2, 1)
-      iex> mat = Result.and_then_x([band_mat, ones], &MatrixReloaded.Matrix.update(&1, &2, {1, 1}))
+      iex> ones = Matrix.new(2, 1)
+      iex> mat = Result.and_then_x([band_mat, ones], &Matrix.update(&1, &2, {1, 1}))
       {:ok,
         [
           [3, 2, 0, 0],
@@ -39,15 +44,15 @@ defmodule MatrixReloaded do
         ]
       }
 
-      iex> mat |> Result.and_then(&MatrixReloaded.Matrix.get_row(&1, 4))
+      iex> mat |> Result.and_then(&Matrix.get_row(&1, 4))
       iex>
       {:error, "You can not get row from the matrix. The row number 4 is outside of matrix!"}
 
-      iex> mat |> Result.and_then(&MatrixReloaded.Matrix.get_row(&1, 3))
+      iex> mat |> Result.and_then(&Matrix.get_row(&1, 3))
       iex>
       {:ok, [3, 2, 0, 0]}
 
-      iex(4)> mat |> Result.and_then(&MatrixReloaded.Matrix.drop_col(&1, 3))
+      iex(4)> mat |> Result.and_then(&Matrix.drop_col(&1, 3))
       {:ok,
         [
           [3, 2, 0],
