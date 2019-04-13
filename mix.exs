3e137c1b8d44ec25f954ad7996de5b446339cb01
Jindrich K. Smitka <jsmitka@smita.info>
2019-04-13 21:57:40 +0200

    Bump version: 2.1.0 → 2.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MatrixReloaded.MixProject do
     [
       app: :matrix_reloaded,
       dialyzer: dialyzer_base() |> dialyzer_ptl(System.get_env("SEMAPHORE_CACHE_DIR")),
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
