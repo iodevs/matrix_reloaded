8c90945b99200eb1bd28330dec2b47e32bcf7f1a
Ondrej Tucek <ondrej.tucek@gmail.com>
2019-04-13 18:19:10 +0200

    Added update function to Vector module

--- lib/matrix_reloaded/vector.ex
+++ lib/matrix_reloaded/vector.ex
@@ -10,6 +10,7 @@ defmodule MatrixReloaded.Vector do
 
   @type t :: [number]
   @type column() :: [[number]]
+  @type subvector :: number | t()
 
   @doc """
   Create a row vector of the specified size. Default values of vector
@@ -273,6 +274,64 @@ defmodule MatrixReloaded.Vector do
   end
 
   @doc """
+  Update row vector by given a row subvector (list) of numbers or just by one number.
+  The vector elements you want to change are given by the index. The index is a
+  non-negative integer and determines the position of the element in the vector.
+
+  Returns result, it means either tuple of `{:ok, vector}` or `{:error, "msg"}`.
+  ##  Example:
+      iex> vec = 0..10 |> Enum.to_list()
+      iex> MatrixReloaded.Vector.update(vec, [0, 0, 0], 5)
+      {:ok, [0, 1, 2, 3, 4, 0, 0, 0, 8, 9, 10]}
+
+      iex> vec = 0..10 |> Enum.to_list()
+      iex> MatrixReloaded.Vector.update(vec, 0, 5)
+      {:ok, [0, 1, 2, 3, 4, 0, 6, 7, 8, 9, 10]}
+  """
+  @spec update(t(), number() | t(), non_neg_integer()) ::
+          Result.t(String.t(), t())
+
+  def update(vec, subvec, index)
+      when is_list(vec) and is_number(subvec) and is_integer(index) do
+    update(vec, [subvec], index)
+  end
+
+  def update(vec, subvec, index)
+      when is_list(vec) and is_list(subvec) and is_integer(index) do
+    len_vec = Kernel.length(vec)
+
+    vec
+    |> is_index_ok?(index)
+    |> Result.and_then(&is_index_at_vector?(&1, len_vec, index))
+    |> Result.and_then(&is_subvec_in_vec?(&1, len_vec, Kernel.length(subvec), index))
+    |> Result.map(&make_update(&1, subvec, index))
+  end
+
+  @doc """
+  Update row vector by given a row subvector (list) of numbers or by one number.
+  The elements you want to change are given by the vector of indices. These
+  indices must be a non-negative integers and determine the positions of
+  the element in the vector.
+
+  Returns result, it means either tuple of `{:ok, vector}` or `{:error, "msg"}`.
+  ##  Example:
+      iex> vec = 0..10 |> Enum.to_list()
+      iex> MatrixReloaded.Vector.update_map(vec, [0, 0], [2, 7])
+      {:ok, [0, 1, 0, 0, 4, 5, 6, 0, 0, 9, 10]}
+
+      iex> vec = 0..10 |> Enum.to_list()
+      iex> MatrixReloaded.Vector.update_map(vec, 0, [2, 7])
+      {:ok, [0, 1, 0, 3, 4, 5, 6, 0, 8, 9, 10]}
+  """
+  @spec update_map(t(), number() | t(), list(non_neg_integer())) ::
+          Result.t(String.t(), t())
+  def update_map(vec, subvec, position_indices) do
+    Enum.reduce(position_indices, {:ok, vec}, fn position, acc ->
+      Result.and_then(acc, &update(&1, subvec, position))
+    end)
+  end
+
+  @doc """
   The size of the vector.
 
   Returns a positive integer.
@@ -288,4 +347,88 @@ defmodule MatrixReloaded.Vector do
   """
   @spec size(t()) :: non_neg_integer
   def size(vec), do: length(vec)
+
+  defp make_update(vec, subvec, index) do
+    len_subvec = Kernel.length(subvec)
+
+    vec
+    |> Enum.with_index()
+    |> Enum.map(fn {val, i} ->
+      if i in index..(index + len_subvec - 1) do
+        subvec |> Enum.at(i - index)
+      else
+        val
+      end
+    end)
+  end
+
+  defp is_index_ok?(vec, idx) when is_integer(idx) and 0 <= idx do
+    Result.ok(vec)
+  end
+
+  defp is_index_ok?(_vec, idx) when is_number(idx) do
+    Result.error("The index must be integer number greater or equal to zero!")
+  end
+
+  defp is_index_at_vector?(
+         vec,
+         len,
+         index,
+         method \\ :update
+       )
+
+  defp is_index_at_vector?(vec, len, idx, _method)
+       when idx <= len do
+    Result.ok(vec)
+  end
+
+  defp is_index_at_vector?(
+         _vec,
+         _len,
+         _idx,
+         method
+       ) do
+    Result.error(
+      "You can not #{Atom.to_string(method)} the #{vec_or_subvec(method)}. The index is outside of vector!"
+    )
+  end
+
+  defp is_subvec_in_vec?(
+         vec,
+         len_vec,
+         len_subvec,
+         index,
+         method \\ :update
+       )
+
+  defp is_subvec_in_vec?(
+         vec,
+         len_vec,
+         len_subvec,
+         idx,
+         _method
+       )
+       when len_subvec + idx <= len_vec do
+    Result.ok(vec)
+  end
+
+  defp is_subvec_in_vec?(
+         _vec,
+         _len_vec,
+         _len_subvec,
+         idx,
+         method
+       ) do
+    Result.error(
+      "You can not #{Atom.to_string(method)} #{vec_or_subvec(method)} on given position #{idx}. A part of subvector is outside of matrix!"
+    )
+  end
+
+  defp vec_or_subvec(:update) do
+    "vector"
+  end
+
+  # defp vec_or_subvec(:get) do
+  #   "subvector or element"
+  # end
 end
